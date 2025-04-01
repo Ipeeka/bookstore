a7cd8e47844fa0157b0a93132c04152937bc4a6e
Ipeeka <pkbrat424@gmail.com>
2025-04-01 09:59:36 +0530

    -

--- client/src/app/components/books/bookmark/bookmark.component.ts
+++ client/src/app/components/books/bookmark/bookmark.component.ts
@@ -6,6 +6,13 @@ import { CommonModule } from '@angular/common';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
+import { MatButtonModule, MatIconButton } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-bookmark',
@@ -17,6 +24,13 @@ import { MatSortModule } from '@angular/material/sort';
     MatTableModule,
     MatPaginatorModule,
     MatSortModule,
+    MatButtonModule,
+    MatIconModule,
+    ButtonModule,
+    InputTextModule,
+    IconFieldModule,
+    InputIconModule,
+    MatTooltipModule
   ],
   templateUrl: './bookmark.component.html',
   styleUrls: ['./bookmark.component.css'],
@@ -25,6 +39,59 @@ export class BookmarkComponent implements OnInit {
   bookmarkedBooks: Book[] = [];
   tableHeaderBgColor: string = '#dddddd';
   constructor(private bookService: BookService) {}
+  statuses: any[] = [
+    {
+      label: 'In Stock',
+      value: 'inStock',
+      bgColor: '#DCFCE7',
+      textColor: '#208646',
+    },
+    {
+      label: 'Low Stock',
+      value: 'lowStock',
+      bgColor: '#FFEDD5',
+      textColor: '#C2440F',
+    },
+    {
+      label: 'Pre-order',
+      value: 'preOrder',
+      bgColor: 'lightblue',
+      textColor: 'black',
+    },
+    {
+      label: 'Out of Stock',
+      value: 'outOfStock',
+      bgColor: '#FEE2E2',
+      textColor: '#B91C1C',
+    },
+  ];
+
+
+  
+  getStatusLabel(status: string): string {
+    const found = this.statuses.find(
+      (s) => s.value.toLowerCase() === status?.toLowerCase().trim()
+    );
+    return found ? found.label : 'Unknown';
+  }
+
+  getStatusStyle(status: string): any {
+    const found = this.statuses.find(
+      (s) => s.value.toLowerCase() === status?.toLowerCase().trim()
+    );
+    return found
+      ? {
+          'background-color': found.bgColor,
+          color: found.textColor,
+          padding: '5px 10px',
+          'border-radius': '10px',
+          'font-weight': 'bold',
+          display: 'inline-block',
+          'min-width': '90px',
+          'text-align': 'center',
+        }
+      : {};
+  }
 
   ngOnInit(): void {
     this.loadBookmarkedBooks();
--- client/src/app/components/common/report/report.component.ts
+++ client/src/app/components/common/report/report.component.ts
@@ -32,8 +32,8 @@ export class ReportComponent implements OnInit {
 
   value = [
     { label: 'Apps', color1: '#34d399', color2: '#fbbf24', value: 25, icon: 'pi pi-table' },
-    { label: 'Messages', color1: '#fbbf24', color2: '#60a5fa', value: 15, icon: 'pi pi-inbox' },
-    { label: 'Media', color1: '#60a5fa', color2: '#c084fc', value: 20, icon: 'pi pi-image' },
+    { label: 'Users', color1: '#fbbf24', color2: '#60a5fa', value: 15, icon: 'pi pi-inbox' },
+    { label: 'Books', color1: '#60a5fa', color2: '#c084fc', value: 20, icon: 'pi pi-image' },
     { label: 'System', color1: '#c084fc', color2: '#c084fc', value: 10, icon: 'pi pi-cog' }
 ];
 
@@ -66,13 +66,13 @@ export class ReportComponent implements OnInit {
         labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
         datasets: [
           {
-            label: 'My First dataset',
+            label: 'Users',
             backgroundColor: documentStyle.getPropertyValue('--p-cyan-500'),
             borderColor: documentStyle.getPropertyValue('--p-cyan-500'),
             data: [65, 59, 80, 81, 56, 55, 40]
           },
           {
-            label: 'My Second dataset',
+            label: 'Books',
             backgroundColor: documentStyle.getPropertyValue('--p-gray-500'),
             borderColor: documentStyle.getPropertyValue('--p-gray-500'),
             data: [28, 48, 40, 19, 86, 27, 90]
